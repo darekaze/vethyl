952d3bf755dec3f797ce0017025351873d65990c
darekaze <darekaze@gmail.com>
2019-10-14 03:42:49 +0800

    Add transaction api

--- client/src/model/transaction.ts
+++ client/src/model/transaction.ts
@@ -1,13 +1,40 @@
-import { action, Action } from 'easy-peasy'
+// This use immer under the hood, so we allow param-reassign
+/* eslint-disable no-param-reassign */
 
-export interface TransactionModel {
-  type: string
+import { action, Action, thunk, Thunk } from 'easy-peasy'
+import api from '../services'
+
+export interface TransactionBody {
+  status: string
   payload: object | null
 }
 
+export interface TransactionModel extends TransactionBody {
+  setResponse: Action<TransactionModel, TransactionBody>
+  updatePayload: Thunk<TransactionModel, TransactionBody>
+}
+
 const transaction: TransactionModel = {
-  type: '',
+  status: '',
   payload: null,
+  setResponse: action((state, data) => {
+    state.status = data.status
+    state.payload = data.payload
+  }),
+  updatePayload: thunk(async (actions, reqBody) => {
+    try {
+      const { data } = await api.post('/transaction', reqBody)
+      actions.updatePayload({
+        status: 'SUCCESS',
+        payload: data,
+      })
+    } catch (err) {
+      actions.updatePayload({
+        status: 'ERROR',
+        payload: err.response.data,
+      })
+    }
+  }),
 }
 
 export default transaction
--- client/src/view/EthTransaction.tsx
+++ client/src/view/EthTransaction.tsx
@@ -1,11 +1,55 @@
-import React from 'react'
-import { Container, Header } from 'semantic-ui-react'
+import React, { useEffect, ChangeEvent } from 'react'
+import { Container, Header, Form } from 'semantic-ui-react'
+import useForm from 'react-hook-form'
+import { useStoreState } from '../hooks'
+
+interface FormData {
+  // startDate: string
+  // endDate: string
+  fromAcct: string
+  toAcct: string
+  // // should be number?
+  // valuePeak: string
+  // valueBottom: string
+}
 
 const EthTransaction: React.FC = () => {
+  const { register, handleSubmit, setValue } = useForm<FormData>()
+  const queryResult = useStoreState(state => state.transaction.payload)
+
+  const handleChange = (e: ChangeEvent, { name, value }: any): void => {
+    setValue(name, value)
+  }
+
+  const onSubmit = handleSubmit(data => {
+    console.log(data)
+  })
+
+  useEffect(() => {
+    register({ name: 'fromAcct' })
+    register({ name: 'toAcct' })
+  })
+
   return (
-    <Container text style={{ marginTop: '7em' }}>
+    <Container text style={{ marginTop: '3rem' }}>
       <Header as="h1">Check Transaction</Header>
-      <p>TBD</p>
+      <Form onSubmit={onSubmit}>
+        <Form.Group widths="equal">
+          <Form.Input
+            name="fromAcct"
+            label="From Account"
+            placeholder="Enter Sender Account"
+            onChange={handleChange}
+          />
+          <Form.Input
+            name="toAcct"
+            label="To Account"
+            placeholder="Enter Receiver Account"
+            onChange={handleChange}
+          />
+        </Form.Group>
+        <Form.Button>Submit</Form.Button>
+      </Form>
     </Container>
   )
 }
