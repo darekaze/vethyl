d5516874e0319ff3a0f34f05880dd95381b565e9
darekaze <darekaze@gmail.com>
2019-10-15 03:33:15 +0800

    Add date range picker component

---
+++ client/src/components/shared/DateRangePicker/index.tsx
@@ -0,0 +1,80 @@
+import React, { useRef, ReactNode } from 'react'
+import DayPickerInput from 'react-day-picker/DayPickerInput'
+import { DayPickerInputProps, DayPickerProps } from 'react-day-picker'
+import 'react-day-picker/lib/style.css'
+
+import { formatDate, parseDate } from 'utils/date'
+import './style.css'
+
+interface DateRangePickerProps {
+  children?: ReactNode
+  getFormValues: Function // useForm types
+  handleFromChange: DayPickerInputProps['onDayChange']
+  handleToChange: DayPickerInputProps['onDayChange']
+}
+
+const FORMAT = 'MM/DD/YYYY'
+
+const DateRangePicker: React.FC<DateRangePickerProps> = ({
+  getFormValues,
+  handleFromChange,
+  handleToChange,
+}) => {
+  // TODO: put local state
+  // TODO: refactor using https://webomnizz.com/change-parent-component-state-from-child-using-hooks-in-react/
+  const toRef = useRef<DayPickerInput>(null)
+
+  const { startDate, endDate } = getFormValues()
+  const modifiers = { start: startDate, end: endDate }
+
+  const onDayClick: DayPickerProps['onDayClick'] = () => {
+    if (toRef && toRef.current) {
+      toRef.current.getInput().focus()
+    }
+  }
+
+  console.log(startDate, endDate)
+
+  return (
+    <div className="InputFromTo">
+      <DayPickerInput
+        value={startDate}
+        placeholder="From"
+        format={FORMAT}
+        formatDate={formatDate}
+        parseDate={parseDate}
+        dayPickerProps={{
+          selectedDays: [startDate, { startDate, endDate }],
+          disabledDays: { after: endDate },
+          toMonth: endDate,
+          modifiers,
+          numberOfMonths: 2,
+          onDayClick,
+        }}
+        onDayChange={handleFromChange}
+      />
+      {' — '}
+      <span className="InputFromTo-to">
+        <DayPickerInput
+          ref={toRef}
+          value={endDate}
+          placeholder="To"
+          format={FORMAT}
+          formatDate={formatDate}
+          parseDate={parseDate}
+          dayPickerProps={{
+            selectedDays: [startDate, { startDate, endDate }],
+            disabledDays: { before: startDate },
+            modifiers,
+            month: startDate,
+            fromMonth: startDate,
+            numberOfMonths: 2,
+          }}
+          onDayChange={handleToChange}
+        />
+      </span>
+    </div>
+  )
+}
+
+export default DateRangePicker
--- client/src/view/EthTransaction.tsx
+++ client/src/view/EthTransaction.tsx
@@ -1,11 +1,15 @@
-import React, { useEffect, ChangeEvent } from 'react'
-import { Container, Header, Form } from 'semantic-ui-react'
+import React, { useEffect } from 'react'
+import { Container, Header, Form, StrictInputProps } from 'semantic-ui-react'
 import useForm from 'react-hook-form'
+import { DayPickerInputProps } from 'react-day-picker'
+import 'react-day-picker/lib/style.css'
+
+import DateRangePicker from 'components/shared/DateRangePicker'
 import { useStoreState } from '../hooks'
 
 interface FormData {
-  // startDate: string
-  // endDate: string
+  startDate: Date
+  endDate: Date
   fromAcct: string
   toAcct: string
   // // should be number?
@@ -14,11 +18,19 @@ interface FormData {
 }
 
 const EthTransaction: React.FC = () => {
-  const { register, handleSubmit, setValue } = useForm<FormData>()
+  const { register, handleSubmit, setValue, getValues } = useForm<FormData>()
   const queryResult = useStoreState(state => state.transaction.payload)
 
-  const handleChange = (e: ChangeEvent, { name, value }: any): void => {
+  const handleChange: StrictInputProps['onChange'] = (e, { name, value }): void => {
     setValue(name, value)
+  }
+
+  const handleFromChange: DayPickerInputProps['onDayChange'] = (day): void => {
+    setValue('startDate', day)
+  }
+
+  const handleToChange: DayPickerInputProps['onDayChange'] = (day): void => {
+    setValue('endDate', day)
   }
 
   const onSubmit = handleSubmit(data => {
@@ -26,6 +38,8 @@ const EthTransaction: React.FC = () => {
   })
 
   useEffect(() => {
+    register({ name: 'startDate' })
+    register({ name: 'endDate' })
     register({ name: 'fromAcct' })
     register({ name: 'toAcct' })
   })
@@ -34,6 +48,12 @@ const EthTransaction: React.FC = () => {
     <Container text style={{ marginTop: '3rem' }}>
       <Header as="h1">Check Transaction</Header>
       <Form onSubmit={onSubmit}>
+        {/* Can put date range picker in one component? */}
+        <DateRangePicker
+          getFormValues={getValues}
+          handleFromChange={handleFromChange}
+          handleToChange={handleToChange}
+        />
         <Form.Group widths="equal">
           <Form.Input
             name="fromAcct"
