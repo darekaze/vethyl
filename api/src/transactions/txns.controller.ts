a22ca28dc6f05b0f08049e8a5e58c2f08fe727a3
darekaze <darekaze@gmail.com>
2020-06-01 20:34:13 +0800

    add verify info to txns query

--- api/src/transactions/txns.controller.ts
+++ api/src/transactions/txns.controller.ts
@@ -1,21 +1,39 @@
 import { Controller, Get, Param, Query } from '@nestjs/common'
-import { ITransaction } from '@vethyl/common'
+import { ITransaction, IFingerprint } from '@vethyl/common'
 import { TransactionsService } from './txns.service'
+import { FingerprintService } from '../fingerprint/fingerprint.service'
 import { TxnsByDateDto } from './dto'
+
+interface TxnsWithFp {
+  data: ITransaction | ITransaction[]
+  fingerprint: IFingerprint | IFingerprint[]
+}
 
 @Controller('txns')
 export class TransactionsController {
-  constructor(private readonly txnsService: TransactionsService) {}
+  constructor(
+    private readonly txnsService: TransactionsService,
+    private readonly fpService: FingerprintService,
+  ) {}
 
   @Get(':hash')
-  async getTransaction(@Param() params: { hash: string }): Promise<ITransaction> {
-    return this.txnsService.findOneByHash(params.hash)
+  async getTransaction(@Param() params: { hash: string }): Promise<TxnsWithFp> {
+    const data = await this.txnsService.findOneByHash(params.hash)
+    const fingerprint = await this.fpService.findOneByNumber(data.blockNumber)
+
+    return { fingerprint, data }
   }
 
   @Get('date')
-  async getTransactionsByDate(
-    @Query() query: TxnsByDateDto,
-  ): Promise<ITransaction[]> {
-    return this.txnsService.findByDate(query)
+  async getTransactionsByDate(@Query() query: TxnsByDateDto): Promise<TxnsWithFp> {
+    const data = await this.txnsService.findByDate(query)
+
+    const last = data.length - 1
+    const fingerprint = await this.fpService.findByBlockRange({
+      start: data[last].blockNumber,
+      end: data[0].blockNumber,
+    })
+
+    return { fingerprint, data }
   }
 }
